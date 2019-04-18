f5f718a7c04b4cddf9438bbfa45657040a7d97ba
Filriya <arizoxppc@gmail.com>
2019-04-18 08:16:48 +0000

    fix some bug

--- bin/setup/install.sh
+++ bin/setup/install.sh
@@ -6,12 +6,17 @@ if [ "$(uname)" == 'Darwin' ]; then
   if ! type "brew" > /dev/null 2>&1; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
-  ./brew/brew.sh
-  ./brew/brew.darwin.sh
+  ./brew/common.sh
+  ./brew/darwin.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   if ! type "brew" > /dev/null 2>&1; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+    # 環境変数の設定
+    test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
   fi
-  ./brew/brew.sh
-  ./brew/brew.linux.sh
+  ./brew/common.sh
 fi
